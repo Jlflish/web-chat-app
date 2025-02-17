41f9765fa5d3a629c59e6e848af0547124cd563d
Jlyfish <FishJeli@Outlook.com>
2025-02-17 16:25:34 +1100

    add messages into db

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,35 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/chat-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
 
 app.get('/', (req, res) => {
   res.send('Chat server is running');
 });
 
+app.get('/messages', async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch messages' });
+  }
+});
+
 io.on('connection', (socket) => {
   console.log('A user connected');
   
-  socket.on('send_message', (message) => {
+  socket.on('send_message', async (message) => {
+    const newMessage = new Message({ content: message });
+    await newMessage.save();
     io.emit('receive_message', message);  // 广播消息
   });
   
